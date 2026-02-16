b705b0f1c1fac5b958c34c3b7d344f2748897c48
Benyamin Shoraki <benyaminchor@gmail.com>
2026-02-16 17:40:53 +0200

    Update .gitignore and refactor order service caching method

--- api_app/app/service/orders_service.py
+++ api_app/app/service/orders_service.py
@@ -1,10 +1,10 @@
 from shared.models import PizzaOrders
-from shared.utils.caching import cache  # הייבוא של הדקורטור
+from shared.utils.caching import cache
 
 
 class OrderService:
 
     @staticmethod
-    @cache(expire=60, prefix="order:")  # <-- הקסם קורה כאן
-    async def get_order_by_id(order_id: str):
+    @cache(expire=60)
+    async def get_order(order_id: str):
         return await PizzaOrders.find_one(PizzaOrders.order_id == order_id)
